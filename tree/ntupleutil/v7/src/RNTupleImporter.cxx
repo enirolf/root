734097d8f546a3009e4fcabc3ed15d3202270a90
Florine de Geus <git@fwdg.nl>
2023-01-26 16:49:57 +0100

    [ntuple] TEMPORARILY subsitute '.' with ':' in branch names

--- tree/ntupleutil/v7/src/RNTupleImporter.cxx
+++ tree/ntupleutil/v7/src/RNTupleImporter.cxx
@@ -223,6 +223,14 @@ ROOT::Experimental::RResult<void> ROOT::Experimental::RNTupleImporter::PrepareSc
          if (isFixedSizeArray)
             fieldType = "std::array<" + fieldType + "," + std::to_string(countval) + ">";
 
+         // If present, remove the trailing dot at the end of a field name. For an explanation as to why some branch names
+         // have this, see https://root.cern/doc/master/classTTree.html#a9193737b5c102ef8d7301410adf73e2f.
+         fieldName.erase(fieldName.find_last_not_of(".") + 1, std::string::npos);
+
+         // Also replace any other occurrences of '.' with ':'.
+         // FIXME this should become a valid C++ valid identifier character.
+         std::replace(fieldName.begin(), fieldName.end(), '.', ':');
+
          RImportField f;
          f.fIsClass = isClass;
          auto fieldOrError = Detail::RFieldBase::Create(fieldName, fieldType);
