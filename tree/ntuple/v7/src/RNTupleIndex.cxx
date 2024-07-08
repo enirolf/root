6081c09e0b388bf839316fb64927fc9cc49e0a17
Florine de Geus <florine.de.geus@cern.ch>
2024-07-08 12:29:08 +0200

    [ntuple] Add initial in-memory index prototype
    This adds (a first version of) the `RNTupleIndex`, which is an
    in-memory structure that maps RNTuple field values (or combinations
    thereof) to an entry index in the RNTuple for which the index was
    built. At this point, the index only resides in memory and thus has
    to be (re)build each time.
    
    `RNTupleIndex` will be used by the  `RNTupleProcessor` to enable
    dataset joins and will be as transparent as possible to users.
    Currently, no public interface is foreseen.

---
+++ tree/ntuple/v7/src/RNTupleIndex.cxx
@@ -0,0 +1,119 @@
+/// \file RNTupleIndex.cxx
+/// \ingroup NTuple ROOT7
+/// \author Florine de Geus <florine.de.geus@cern.ch>
+/// \date 2024-04-02
+/// \warning This is part of the ROOT 7 prototype! It will change without notice. It might trigger earthquakes. Feedback
+/// is welcome!
+
+/*************************************************************************
+ * Copyright (C) 1995-2024, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#include <ROOT/RNTupleIndex.hxx>
+
+namespace {
+ROOT::Experimental::Internal::RNTupleIndex::NTupleIndexValue_t
+CastValuePtr(void *valuePtr, const ROOT::Experimental::RFieldBase &field)
+{
+   ROOT::Experimental::Internal::RNTupleIndex::NTupleIndexValue_t value;
+
+   switch (field.GetValueSize()) {
+   case 1: value = *reinterpret_cast<std::uint8_t *>(valuePtr); break;
+   case 2: value = *reinterpret_cast<std::uint16_t *>(valuePtr); break;
+   case 4: value = *reinterpret_cast<std::uint32_t *>(valuePtr); break;
+   case 8: value = *reinterpret_cast<std::uint64_t *>(valuePtr); break;
+   default: throw ROOT::Experimental::RException(R__FAIL("value size not supported"));
+   }
+
+   return value;
+}
+} // anonymous namespace
+
+ROOT::Experimental::Internal::RNTupleIndex::RNTupleIndex(std::vector<std::unique_ptr<RFieldBase>> &fields,
+                                                         NTupleSize_t nEntries)
+   : fIndexFields(std::move(fields))
+{
+   std::vector<RFieldBase::RValue> fieldValues;
+   fieldValues.reserve(fIndexFields.size());
+   for (const auto &field : fIndexFields) {
+      if (!field->IsSimple() || field->GetTypeName() == "float" || field->GetTypeName() == "double") {
+         throw RException(R__FAIL("Cannot use field \"" + field->GetFieldName() + "\" with type \"" +
+                                  field->GetTypeName() + "\" for indexing. Only integral types are allowed."));
+      }
+      fieldValues.emplace_back(field->CreateValue());
+   }
+
+   std::vector<NTupleIndexValue_t> indexValues;
+   indexValues.reserve(fIndexFields.size());
+
+   for (unsigned i = 0; i < nEntries; ++i) {
+      indexValues.clear();
+      for (auto &fieldValue : fieldValues) {
+         // TODO(fdegeus): use bulk reading
+         fieldValue.Read(i);
+
+         auto valuePtr = fieldValue.GetPtr<void>();
+         indexValues.push_back(CastValuePtr(valuePtr.get(), fieldValue.GetField()));
+      }
+      fIndex[RIndexValue(indexValues)].push_back(i);
+   }
+}
+
+std::unique_ptr<ROOT::Experimental::Internal::RNTupleIndex>
+ROOT::Experimental::Internal::RNTupleIndex::Create(const std::vector<std::string> &fieldNames, RPageSource &pageSource)
+{
+   pageSource.Attach();
+   auto desc = pageSource.GetSharedDescriptorGuard();
+
+   std::vector<std::unique_ptr<RFieldBase>> fields;
+   fields.reserve(fieldNames.size());
+
+   for (const auto &fieldName : fieldNames) {
+      auto fieldId = desc->FindFieldId(fieldName);
+      if (fieldId == kInvalidDescriptorId)
+         throw RException(R__FAIL("Could not find field \"" + std::string(fieldName) + "."));
+
+      const auto &fieldDesc = desc->GetFieldDescriptor(fieldId);
+      auto field = fieldDesc.CreateField(desc.GetRef());
+
+      CallConnectPageSourceOnField(*field, pageSource);
+
+      fields.push_back(std::move(field));
+   }
+
+   return std::unique_ptr<RNTupleIndex>(new RNTupleIndex(fields, pageSource.GetNEntries()));
+}
+
+ROOT::Experimental::NTupleSize_t
+ROOT::Experimental::Internal::RNTupleIndex::GetFirstEntryNumber(const std::vector<void *> &valuePtrs) const
+{
+   const auto entryIndices = GetAllEntryNumbers(valuePtrs);
+   if (!entryIndices)
+      return kInvalidNTupleIndex;
+   return entryIndices->front();
+}
+
+const std::vector<ROOT::Experimental::NTupleSize_t> *
+ROOT::Experimental::Internal::RNTupleIndex::GetAllEntryNumbers(const std::vector<void *> &valuePtrs) const
+{
+   if (valuePtrs.size() != fIndexFields.size())
+      throw RException(R__FAIL("Number of value pointers must match number of indexed fields."));
+
+   std::vector<NTupleIndexValue_t> indexValues;
+   indexValues.reserve(fIndexFields.size());
+
+   for (unsigned i = 0; i < valuePtrs.size(); ++i) {
+      indexValues.push_back(CastValuePtr(valuePtrs[i], *fIndexFields[i]));
+   }
+
+   auto entryNumber = fIndex.find(RIndexValue(indexValues));
+
+   if (entryNumber == fIndex.end())
+      return nullptr;
+
+   return &(entryNumber->second);
+}
