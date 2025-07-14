a543fb3fb5f7dbb88cf8fe3e08b79c7339ccde5f
Florine de Geus <florine.de.geus@cern.ch>
2025-07-14 17:14:46 +0200

    [df] Allow snapshotting from TTree to RNTuple

--- tree/dataframe/inc/ROOT/RDF/RInterface.hxx
+++ tree/dataframe/inc/ROOT/RDF/RInterface.hxx
@@ -1345,11 +1345,6 @@ public:
       RResultPtr<RInterface<RLoopManager>> resPtr;
 
       if (options.fOutputFormat == ESnapshotOutputFormat::kRNTuple) {
-         if (RDFInternal::GetDataSourceLabel(*this) == "TTreeDS") {
-            throw std::runtime_error("Snapshotting from TTree to RNTuple is not yet supported. The current recommended "
-                                     "way to convert TTrees to RNTuple is through the RNTupleImporter.");
-         }
-
          // The data source of the RNTuple resulting from the Snapshot action does not exist yet here, so we create one
          // without a data source for now, and set it once the actual data source can be created (i.e., after
          // writing the RNTuple).
